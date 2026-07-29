b379c6b1c5976faf51fed7597c64e02a186c259d
Jit2005git <jitghosh304@gmail.com>
2026-07-30 00:28:38 +0530

    about

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Hello user! This is my first flask app'
+
+@app.route('/about')
+def about():
+    return 'This is about us page'
+@app.route('/contact')
+def contact():
+    return 'This is contact us page'
