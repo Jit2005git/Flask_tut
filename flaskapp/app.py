dd29d25d208d213f058933381535612e9fdc71be
Jit2005git <jitghosh304@gmail.com>
2026-08-05 00:22:56 +0530

    a6

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -9,7 +9,15 @@ def login():
 def submit():
     username = request.form.get("username")
     password = request.form.get("password")
-    if username == "Jit123" and password == "password":
+    # if username == "Jit123" and password == "password":
+    #     return render_template("welcome.html", name=username)
+    valid_users={
+        'Jit123': 'password',
+        'user1': 'pass1',
+        'user2': 'pass2',
+        'dev': 'jg'
+    }
+    if username in valid_users and valid_users[username] == password:
         return render_template("welcome.html", name=username)
     else:
         return "Invalid credentials"
