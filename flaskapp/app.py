0f0da2e4a83f038ff525905226a4f9e2fe5b5298
Jit2005git <jitghosh304@gmail.com>
2026-08-05 00:16:15 +0530

    app4

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, request
 app=Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("home.html")
-@app.route("/submit", methods=["POST"])
 def login():
+    return render_template("login.html")
+@app.route("/submit", methods=["POST"])
+def submit():
     username = request.form.get("username")
     password = request.form.get("password")
+    if username == "Jit123" and password == "password":
+        return render_template("welcome.html", name=username)
+    else:
+        return "Invalid credentials"
